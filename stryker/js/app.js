dee041d55bc518b1fb95eaa5f3410da88e3e3099
PineappleRind <pineapplerind.info@gmail.com>
2020-12-03 15:56:32 -0500

    mobile compatibillity

--- stryker/js/app.js
+++ stryker/js/app.js
@@ -1,6 +1,6 @@
 let n = document.querySelector("main");
 let s = document.querySelector("nav");
-let r, t;
+let r, t, c;
 t = document.querySelector(".nav-toggle");
 r = {
     d: document.querySelector(".nav-toggle"),
@@ -9,7 +9,7 @@ r = {
     }
 };
 
-
+c = document.querySelector(".nav-mobile");
 
 let f = document.querySelector(".footer");
 
@@ -17,14 +17,20 @@ function showFooter() {
     f.style.display = 'block';
 }
 
+function nav() {
+    if (c.style.width !== '100%') {
+       c.style.width = '100%';
+    } else {
+        c.style.width = '0%';
+    }
+}
+
 setTimeout(function() {
     showFooter();
 }, 800)
 
-if (n.scrollTop >= s.scrollHeight) {
-    s.style.backgroundColor = 'rgba(0,0,0,0.7)';
-}
 
 t.addEventListener("click", function(e){
     r.l(e);
+    nav();
 })
