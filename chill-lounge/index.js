344308669b0f18928bc5ee6e146d715c8e019b23
PineappleRind <pineapplerind.info@gmail.com>
2021-01-19 13:13:52 -0500

    chill lounge v3

--- chill-lounge/index.js
+++ chill-lounge/index.js
@@ -5,6 +5,15 @@ function linkConfirm(link) {
     modal.style.display = 'flex';
     setTimeout(function(){
         modal.style.transform = 'scale(1.1)'
+        modal.style.opacity = '1'
     },10)
     modalButton.setAttribute('onclick', "window.location.href = '" + link + "'");
+}
+
+function linkClose() {
+    modal.style.transform = 'scale(1.4)'
+    modal.style.opacity = '0'
+    setTimeout(function(){
+        modal.style.display = 'none';
+    },300)
 }
