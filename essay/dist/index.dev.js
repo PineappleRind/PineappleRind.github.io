19e14980871ef2d705cf9f6cefd9fd8f4a916760
PineappleRind <pineapplerind.info@gmail.com>
2021-02-09 21:12:28 -0500

    push

--- essay/dist/index.dev.js
+++ essay/dist/index.dev.js
@@ -4,17 +4,52 @@ function $(id) {
   return document.getElementById(id);
 }
 
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+
+  return "";
+}
+
 var input = $('dark');
 var theBody = $('body');
+var darkmodebool = false;
 
 function darkmode() {
   if (input.checked === false) {
     document.body.classList.remove('dark');
+    darkmodebool = false;
   } else {
     document.body.classList.add('dark');
+    darkmodebool = true;
   }
 }
 
 input.onclick = function () {
   darkmode();
+  setCookie('darkmodetrue', darkmodebool);
 };
+
+if (getCookie('darkmodetrue') === 'true') {
+  document.body.classList.add('dark');
+}
--- essay/index.js
+++ essay/index.js
@@ -1,6 +1,7 @@
 function $(e) {
     return document.getElementById(e)
 }
+<<<<<<< Updated upstream
 let o = window,
     i = $("dark"),
     b = $("body");
@@ -17,3 +18,49 @@ i.onclick = (() => {
         n = parseInt(t / (d - e) * 100);
     $("progress").style.width = n + "%"
 });
+=======
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+let input = $('dark')
+let theBody = $('body')
+let darkmodebool = false
+function darkmode() {
+    if (input.checked === false) {
+        document.body.classList.remove('dark');
+        darkmodebool = false
+    } else {
+        document.body.classList.add('dark')
+        darkmodebool = true
+    }
+}
+
+input.onclick = () => {
+    darkmode()
+    setCookie('darkmodetrue', darkmodebool);
+}
+
+if (getCookie('darkmodetrue') === 'true') {
+    document.body.classList.add('dark')
+}
+>>>>>>> Stashed changes
