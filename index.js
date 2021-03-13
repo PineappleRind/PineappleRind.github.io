85bb24739a99cdeb65b2eb363df59aabca29e106
PineappleRind <Guest@St-Joseph-Homeschools-MacBook-Air.local>
2021-03-13 13:15:32 -0500

    pu sh

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ function $(id) {
 for (let j = 0; j < $("btn").length; j++) {
   setTimeout(function(){
       setTimeout(function(){
-          $("btn")[j].style.opacity = '1'
-          $("btn")[j].style.transform = 'translateY(0px)'
-          setTimeout(function(){
-              $("btn")[j].style.transition = '0.3s'
-          },500 * (j + 1))
+        $("btn")[j].classList.remove('hidden')
       },500 * j)
   },1000)
 }
