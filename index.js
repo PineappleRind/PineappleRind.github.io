2451c5eb7de9c2ccf81d2191d3eb318c453a4334
PineappleRind <pineapplerind.info@gmail.com>
2021-03-14 09:03:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ function $(id) {
     return document.getElementsByClassName(id)
 }
 
-for (let j = 0; j < $("btn").length; j++) {
+onchange = () => {
+    for (let j = 0; j < $("btn").length; j++) {
   setTimeout(function(){
       setTimeout(function(){
         $("btn")[j].classList.remove('hidden')
       },500 * j)
   },1000)
+}
+}
+
+onload = () => {
+    for (let j = 0; j < $("btn").length; j++) {
+  setTimeout(function(){
+      setTimeout(function(){
+        $("btn")[j].classList.remove('hidden')
+      },500 * j)
+  },1000)
+}
 }
 
 function init(){
