9a1de240e45fe3b4f30e4b17126879c50cc9565c
PineappleRind <pineapplerind.info@gmail.com>
2021-03-17 12:25:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,18 @@ function loadBtns() {
         },1000)
     }
 }
+      barba.init({
+        transitions: [{
+          name: 'opacity-transition',
+          leave(data) {
+            return gsap.to(data.current.container, {
+              opacity: 0
+            });
+          },
+          enter(data) {
+            return gsap.from(data.next.container, {
+              opacity: 0
+            });
+          }
+        }]
+      });
