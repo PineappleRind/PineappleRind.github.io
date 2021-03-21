9d9076af1c4e1069ac1e968fba60630e0a58d3c4
PineappleRind <pineapplerind.info@gmail.com>
2021-03-20 20:34:52 -0400

    Update log.js

--- log.js
+++ log.js
@@ -1,11 +1,11 @@
-function sendMessage(username,content) {
+function sendMessage(content) {
       var request = new XMLHttpRequest();
       request.open("POST", "https://discord.com/api/webhooks/822978952368619552/aMEMuYmAHPOGAgBtFWXSQvHtq8ZmvqlrbGewE9eItvESCRaxkHfoC6h8TeXYxZ6K1wgX");
 
       request.setRequestHeader('Content-type', 'application/json');
 
       var params = {
-        username: username,
+        username: "Logger",
         avatar_url: "",
         content: content
       }
@@ -13,12 +13,4 @@ function sendMessage(username,content) {
       request.send(JSON.stringify(params));
 }
 
-function text(url) {
-  return fetch(url).then(res => res.text());
-}
-
-text('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
-  let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/
-  let ip = data.match(ipRegex)[0];
-      setTimeout(function(){ sendMessage('New Visitor!', `IP address: ${ip}`)},1000)
-});
+sendMessage('New Visitor!')
