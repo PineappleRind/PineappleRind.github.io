de966c118ced1907105f66d4ee3aab006240c269
PineappleRind <pineapplerind.info@gmail.com>
2021-03-22 15:51:12 -0400

    minify

--- stuff/memelist/index.js
+++ stuff/memelist/index.js
@@ -1,16 +1,17 @@
 var bod = document.querySelector("#bod");
 let things = ["Zhou", "Scuzi", "Grahy", "Heather's Kitchen Party", "Bad dad ad that make me sad, i am crab with a pad", "Fat Christina", "Maryolin!", "Ma name's BRITNEY", "Fabian", "At the business centre, Mike Hepple", "Marcello & Vivian Murphy", "Maurice", "John & Gladys", "Deep Dave", "Grapey", "C-H-E-A-P-O-U-L-E-A-R-N-I cheapoulearni", "Hoooooie", "Canala! Canala!", "TITONGO GET NO PAY", "Chocolo-fudge peanuto-caramel", "Andrew Chester", "Region of Prescott", "Rambleberry", "Aureara Borealis", "Guy Sax"];
-
+function $(e) {
+    return document.getElementById(e)
+}
 function mergeSort(e) {
     let t = e.length / 2;
-    if (e.length < 2) return e;
-    return merge(mergeSort(e.splice(0, t)), mergeSort(e))
+    return e.length < 2 ? e : merge(mergeSort(e.splice(0, t)), mergeSort(e))
 }
 
 function merge(e, t) {
-    let a = [];
-    for (; e.length && t.length;) e[0] < t[0] ? a.push(e.shift()) : a.push(t.shift());
-    return [...a, ...e, ...t]
+    let n = [];
+    for (; e.length && t.length;) e[0] < t[0] ? n.push(e.shift()) : n.push(t.shift());
+    return [...n, ...e, ...t]
 }
 let sorted = mergeSort(things);
 it = "<ul><h1>Meme List</h1><hr></hr>" + sorted.map(function(e) {
@@ -19,9 +20,9 @@ it = "<ul><h1>Meme List</h1><hr></hr>" + sorted.map(function(e) {
 for (let e = 0; e < document.getElementsByClassName("item").length; e++) {
     console.log("ayo");
     let t = document.getElementsByClassName("item")[e],
-        a = t.firstElementChild;
-    a.onclick = (() => {
-        removeElement(a.parentElement), sendMessage(`User deleted ${t.textContent}`)
+        n = t.firstElementChild;
+    n.onclick = (() => {
+        removeElement(n.parentElement), sendMessage(`User deleted ${t.textContent}`)
     })
 }
 
@@ -34,30 +35,21 @@ function removeElement(e) {
 function sendMessage(e) {
     var t = new XMLHttpRequest;
     t.open("POST", "https://discord.com/api/webhooks/822978952368619552/aMEMuYmAHPOGAgBtFWXSQvHtq8ZmvqlrbGewE9eItvESCRaxkHfoC6h8TeXYxZ6K1wgX"), t.setRequestHeader("Content-type", "application/json");
-    var a = {
+    var n = {
         username: "Logger: Meme List",
         avatar_url: "",
         content: e
     };
-    t.send(JSON.stringify(a))
+    t.send(JSON.stringify(n))
 }
-
-
-document.getElementById('open').onclick = () => {
-    document.getElementById('modal').style.transform='scaley(1)'
-    document.getElementById('modal').style.top='0'
-}
-
-document.getElementById('close').onclick = () => {
-    document.getElementById('modal').style.transform='scaley(0)'
-    document.getElementById('modal').style.top='100%'
-}
-let snackbar = document.getElementById('snackbar')
-document.getElementById('submit').onclick = () => {
-    let submit = document.getElementById('input').value
-    sendMessage(`User submitted meme: ${submit}`)
-    snackbar.classList.add('showing')
-    setTimeout(function(){
-        snackbar.classList.remove('showing')
-    },3000)
-}
+document.getElementById("open").onclick = (() => {
+    document.getElementById("modal").style.transform = "scaley(1)", document.getElementById("modal").style.top = "0"
+}), document.getElementById("close").onclick = (() => {
+    document.getElementById("modal").style.transform = "scaley(0)", document.getElementById("modal").style.top = "100%"
+});
+let snackbar = document.getElementById("snackbar");
+document.getElementById("submit").onclick = (() => {
+    sendMessage(`User submitted meme: ${document.getElementById("input").value}`), snackbar.classList.add("showing"), setTimeout(function() {
+        snackbar.classList.remove("showing")
+    }, 3e3)
+});
