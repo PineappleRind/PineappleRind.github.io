959702859bba3d911797e5f51b6f9e3287d07e21
PineappleRind <pineapplerind.info@gmail.com>
2021-03-23 12:29:13 -0400

    pu sh

--- index.js
+++ index.js
@@ -1,32 +1,55 @@
 function $(id) {
-    return document.getElementsByClassName(id)
+  return document.getElementsByClassName(id)
 }
 
+let home = `
+                <h1>PineappleRind</h1>
+                <h3>frontend web developer</h3>
+                <div class="button-wrap">
+                    <a href="experience.html" class="btn hidden">EXPERIENCE</a>
+                    <a href="projects.html" class="btn hidden">PROJECTS</a>
+                    <a class="btn hidden">CONTACT</a>
+                    </div>
+`
+
+let projects = `
+    <h1>Projects</h1>
+    <h3>working on this one</h3>
+    <div class="button-wrap">
+        <a href="/" class="btn hidden">HOME</a>
+        <a href="experience.html" class="btn hidden">EXPERIENCE</a>
+        <a class="btn hidden">CONTACT</a>
+        </div>`
+
+let experience = `
+            <h1>Experience</h1>
+            <ul>
+                <li>2 years of experience in HTML</li>
+                <li>1 year of experience in JavaScript & CSS</li>
+                <li>6 months of experience with React & Node.js</li>
+                <li>2 months of experience with Next.js & tailwindcss</li>
+            </ul>
+            <div class="button-wrap">
+                <a onclick="changeContent(home)" class="btn hidden">HOME</a>
+                <a class="btn hidden">PROJECTS</a>
+                <a class="btn hidden">CONTACT</a>
+                </div>`
+
+        function changeContent(e) {
+          let cont = document.querySelector('.flex-left')
+          cont.innerHTML = ''
+          cont.innerHTML = e
+        }
 onload = () => {
-    loadBtns()
+  loadBtns()
 }
 
 function loadBtns() {
-    for (let j = 0; j < $("btn").length; j++) {
-        setTimeout(function(){
-            setTimeout(function(){
-                $("btn")[j].classList.remove('hidden')
-            },500 * j)
-        },1000)
-    }
+  for (let j = 0; j < 3; j++) {
+      setTimeout(function(){
+          setTimeout(function(){
+              $("btn")[j].classList.remove('hidden')
+          },500 * j)
+      },1000)
+  }
 }
-      barba.init({
-        transitions: [{
-          name: 'opacity-transition',
-          leave(data) {
-            return gsap.to(data.current.container, {
-              opacity: 0
-            });
-          },
-          enter(data) {
-            return gsap.from(data.next.container, {
-              opacity: 0
-            });
-          }
-        }]
-      });
