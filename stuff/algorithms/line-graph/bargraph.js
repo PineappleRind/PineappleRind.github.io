46a7fe09720698705a5a8f26c75a37f85d772a94
PineappleRind <pineapplerind.info@gmail.com>
2021-03-29 10:13:18 -0400

    fix error "unexpected end of input"

--- stuff/algorithms/line-graph/bargraph.js
+++ stuff/algorithms/line-graph/bargraph.js
@@ -10,19 +10,21 @@ let canvas = document.getElementById("canvas"),
     equationY = document.getElementById('equationY').value;
 
 function draw(t, e) {
-    setTimeout(function(){let a = new Path2D;
-a.arc(t, e, parseInt(document.getElementById("radius").value), 0, 3 * Math.PI, !1), ctx.fillStyle = "white", ctx.fill(a), ctx.strokeStyle = "white", ctx.stroke(a);},0.1)
+let a = new Path2D;
+a.arc(t, e, parseInt(document.getElementById("radius").value), 0, 3 * Math.PI, false)
+ctx.fillStyle = "white", ctx.fill(a), ctx.strokeStyle = "white", ctx.stroke(a);
 }
 
 function restart() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height), x = 0, y = 0, t = 0;
-    for(i=0; i<10000; i++)
-{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    x = 0, y = 0, t = 0;
+    for(i=0; i<10000; i++) {
     window.clearInterval(i);
-}
+    }
+
 setTimeout(function(){
-    setInterval(function(){increment()}, speed);
-},100)
+    setInterval(function(){increment()},speed)},100)
+}
 function increment() {
     y = canvas.height / 2 - 200 * eval(equationY);
     x = canvas.width / 2 - 300 * Math.sin(t);
@@ -35,20 +37,22 @@ function open(t) {
 function close(t) {
     t.style.opacity = 0, t.style.transform = "translatey(300px) scale(0.5,0.5)"
 }
-canvas.width = 1408, canvas.height = 739 * 1.1
+canvas.width = 1408
+canvas.height = 739 * 1.1;
+
 setInterval(function(){increment()}, speed)
 
-oninput = (() => {
+oninput = () => {
     radius = parseInt(document.getElementById("radius").value);
     speed = parseInt(document.getElementById('speed').value);
-    equationY = document.getElementById('equationY').value
+    equationY = document.getElementById('equationY').value;
      restart()
-})
+}
 
-document.getElementById("open").onclick = (() => {
+document.getElementById("open").onclick = () => {
     open(document.querySelector(".controls"))
-})
+}
 
-document.getElementById("close").onclick = (() => {
+document.getElementById("close").onclick = () => {
     close(document.querySelector(".controls"))
-})
+}
