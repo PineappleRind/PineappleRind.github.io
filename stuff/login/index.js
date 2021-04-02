489462b206817334a990d06d2a8e3a85b7b6f928
PineappleRind <pineapplerind.info@gmail.com>
2021-04-02 15:31:40 -0400

    minesweeper start...

--- stuff/login/index.js
+++ stuff/login/index.js
@@ -24,7 +24,7 @@ document.getElementById('date').innerHTML = `${weekdayFinal} ${theMonth} ${cDay}
 
 document.getElementById('dateMenu').innerHTML = `${weekdayFinal} ${theMonth} ${cDay}`
 document.getElementById('timeMenu').innerHTML = `${toRegularTime(time)}`
-},100)
+},1000)
 
 function $(t) {return document.getElementById(t)}
 
@@ -128,18 +128,7 @@ setTimeout(function(){
   }
 }*/
 let increment = 1
-function dialog() {
-  let html = `
-  <div class="dialog" style="left: ${(increment * 20) + 200}px; top: ${((increment + 30)* (increment/2))+ 300}px" id="dialog${increment}">
-    <h1 id="dialog-header-${increment}" class="header">App... not found.</h1>
-    <div class="dialog-main">
-      <p>I didn't code Finder, maybe I will in the future?</p>
-      <div class="dialog-buttons">
-        <button class="contained" id="ok${increment}">OK</button>
-      </div>
-    </div>
-  </div>
-  `
+function dialog(html) {
 
   document.body.insertAdjacentHTML('beforeend',html)
   setTimeout(function(){
@@ -154,14 +143,35 @@ function dialog() {
   },10)
 }
 
-$('finder').onclick = () => {dialog()}
-function wait(e,r){
-  setTimeout(function(){
-    e;
-  },r)
+function openLaunchpad() {
+
 }
+
+$('finder').onclick = () => {dialog(`<div class="dialog" style="left: ${(increment * 20) + 200}px; top: ${((increment + 30)* (increment/2))+ 300}px" id="dialog${increment}"><h1 id="dialog-header-${increment}" class="header">App... not found.</h1><div class="dialog-main"><p>I didn't code Finder, maybe I will in the future?</p><div class="dialog-buttons"><button class="contained" id="ok${increment}">OK</button></div></div></div>`)}
 $('launchpad').onclick = () => {
-  openLaunchpad
+  openLaunchpad()
+}
+let minesweeperI = 0
+$('minesweeper').onclick = () => {
+  if (minesweeperI > 0) {
+    return;
+  } else {
+    dialog(`<div class="system-window" id="dialog${increment}" style="left: ${(increment * 20)}px; top: ${((increment + 30)* (increment/2)) + 30}px">
+    <h1 id="dialog-header-${increment}">Minesweeper</h1>
+    <div id="minesweeper-box">
+        <h2>MINESWEEPER!</h2><hr><br><br>
+        <div id="field"></div>
+        <br>
+        <div id="lost" style="display: none;">
+        <h3>You got bombed!</h3>
+        <button id="new-game-button" type="button" onclick="reload()">Start Again</button>
+        </div>         
+    </div>
+    </div>`)
+    document.getElementById('lost').style.display="none";
+    startGame();
+    minesweeperI++
+  }
 }
 document.querySelector('.close-notif').onclick = () => {
   let u = document.querySelector('.close-notif').parentElement
@@ -187,7 +197,7 @@ for (let i = 0; i < document.getElementsByClassName("app-container").length; i++
 }
 
 for (let i = 0; i < document.getElementsByClassName('dialog').length;i++) {
-  
+
 }
 var headerNum = 0;
 function dragElement(e,r) {
@@ -218,4 +228,221 @@ function dragElement(e,r) {
     document.getElementsByClassName("header")[headerNum].onmousedown = l;
   }
   console.log(headerNum)
+}
+
+
+/****************
+ Minesweeper Game by OpenGenus IQ
+*****************/
+
+var components = {
+  num_of_rows : 12,
+  num_of_cols : 24,
+  num_of_bombs : 55,
+  bomb : '💣',
+  alive : true,
+  colors : {1: 'blue', 2: 'green', 3: 'red', 4: 'purple', 5: 'maroon', 6: 'turquoise', 7: 'black', 8: 'grey'}
+}
+
+function startGame() {
+  components.bombs = placeBombs();
+  document.getElementById('field').appendChild(createTable());
+}
+
+function placeBombs() {
+  var i, rows = [];
+  
+  for (i=0; i<components.num_of_bombs; i++) {
+      placeSingleBomb(rows);
+  }
+  return rows;
+} 
+
+function placeSingleBomb(bombs) {
+
+  var nrow, ncol, row, col;
+  nrow = Math.floor(Math.random() * components.num_of_rows);
+  ncol = Math.floor(Math.random() * components.num_of_cols);
+  row = bombs[nrow];
+  
+  if (!row) {
+      row = [];
+      bombs[nrow] = row;
+  }
+  
+  col = row[ncol];
+  
+  if (!col) {
+      row[ncol] = true;
+      return
+  } 
+  else {
+      placeSingleBomb(bombs);
+  }
+}
+
+function cellID(i, j) {
+  return 'cell-' + i + '-' + j;
+}
+
+function createTable() {
+  var table, row, td, i, j;
+  table = document.createElement('table');
+  
+  for (i=0; i<components.num_of_rows; i++) {
+      row = document.createElement('tr');
+      for (j=0; j<components.num_of_cols; j++) {
+          td = document.createElement('td');
+          td.id = cellID(i, j);
+          row.appendChild(td);
+          addCellListeners(td, i, j);
+      }
+      table.appendChild(row);
+  }
+  return table;
+}
+
+function addCellListeners(td, i, j) {
+  td.addEventListener('mousedown', function(event) {
+      if (!components.alive) {
+          return;
+      }
+      components.mousewhiches += event.which;
+      if (event.which === 3) {
+          return;
+      }
+      if (this.flagged) {
+          return;
+      }
+      this.style.backgroundColor = 'lightGrey';
+  });
+
+  td.addEventListener('mouseup', function(event) {
+    
+      if (!components.alive) {
+          return;
+      }
+
+      if (this.clicked && components.mousewhiches == 4) {
+          performMassClick(this, i, j);
+      }
+
+      components.mousewhiches = 0;
+      
+      if (event.which === 3) {
+         
+          if (this.clicked) {
+              return;
+          }
+          if (this.flagged) {
+              this.flagged = false;
+              this.textContent = '';
+          } else {
+              this.flagged = true;
+              this.textContent = components.flag;
+          }
+
+          event.preventDefault();
+          event.stopPropagation();
+        
+          return false;
+      } 
+      else {
+          handleCellClick(this, i, j);
+      }
+  });
+
+  td.oncontextmenu = function() { 
+      return false; 
+  };
+}
+
+function handleCellClick(cell, i, j) {
+  if (!components.alive) {
+      return;
+  }
+
+  if (cell.flagged) {
+      return;
+  }
+
+  cell.clicked = true;
+
+  if (components.bombs[i][j]) {
+      cell.style.color = 'red';
+      cell.textContent = components.bomb;
+      gameOver();
+      
+  }
+  else {
+      cell.style.backgroundColor = 'lightGrey';
+      num_of_bombs = adjacentBombs(i, j);
+      if (num_of_bombs) {
+          cell.style.color = components.colors[num_of_bombs];
+          cell.textContent = num_of_bombs;
+      } 
+      else {
+          clickAdjacentBombs(i, j);
+      }
+  }
+}
+
+function adjacentBombs(row, col) {
+  var i, j, num_of_bombs;
+  num_of_bombs = 0;
+
+  for (i=-1; i<2; i++) {
+      for (j=-1; j<2; j++) {
+          if (components.bombs[row + i] && components.bombs[row + i][col + j]) {
+              num_of_bombs++;
+          }
+      }
+  }
+  return num_of_bombs;
+}
+
+function adjacentFlags(row, col) {
+  var i, j, num_flags;
+  num_flags = 0;
+
+  for (i=-1; i<2; i++) {
+      for (j=-1; j<2; j++) {
+          cell = document.getElementById(cellID(row + i, col + j));
+          if (!!cell && cell.flagged) {
+              num_flags++;
+          }
+      }
+  }
+  return num_flags;
+}
+
+function clickAdjacentBombs(row, col) {
+  var i, j, cell;
+  
+  for (i=-1; i<2; i++) {
+      for (j=-1; j<2; j++) {
+          if (i === 0 && j === 0) {
+              continue;
+          }
+          cell = document.getElementById(cellID(row + i, col + j));
+          if (!!cell && !cell.clicked && !cell.flagged) {
+              handleCellClick(cell, row + i, col + j);
+          }
+      }
+  }
+}
+
+function performMassClick(cell, row, col) {
+  if (adjacentFlags(row, col) === adjacentBombs(row, col)) {
+      clickAdjacentBombs(row, col);
+  }
+}
+
+function gameOver() {
+  components.alive = false;
+  document.getElementById('lost').style.display="block";
+}
+
+function reload(){
+  window.location.reload();
 }
