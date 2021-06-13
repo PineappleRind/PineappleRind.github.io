645b16cb006e898f70c4854b6a8f807fee4c07fe
northernlights03 <65303441+northernlights03@users.noreply.github.com>
2021-06-13 14:43:35 -0400

    one more push

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+var backgrounds = [// PRUDENT
+  `
+  linear-gradient(rgb(4, 0, 227), transparent),
+  linear-gradient(to top left, rgb(128, 0, 255), transparent),
+  linear-gradient(to top right, rgb(0, 121, 196), transparent), black
+  `, // Red/orange
+  `
+  linear-gradient(rgb(227, 0, 4), transparent),
+  linear-gradient(to top left, rgb(255, 0, 128), transparent),
+  linear-gradient(to top right, rgb(196, 121, 0), transparent), black
+  `, // Green/blue
+  `
+  linear-gradient(rgb(4, 100, 0), transparent),
+  linear-gradient(to top left, rgb(0, 128, 255), transparent),
+  linear-gradient(to top right, rgb(0, 196, 121), transparent), black
+  `, // Yellow/green
+  `
+  linear-gradient(rgb(4, 100, 0), transparent),
+  linear-gradient(to top left, rgb(128, 130, 0), transparent),
+  linear-gradient(to top right, rgb(0, 170, 121), transparent), black
+  `,
+  // Yellow/orange
+  `
+  linear-gradient(rgb(180, 160, 0), transparent),
+  linear-gradient(to top left, rgb(255, 80, 0), transparent),
+  linear-gradient(to top right, rgb(230, 170, 0), transparent), black
+  `
+]
+
 function $(t) {
   return document.querySelectorAll(t)
 }
@@ -8,19 +37,43 @@ for (let t = 0; t < 3; t++) {
   })
 }
 
-function classAdj(t) {
+function classAdj(t) { // t paramater is the card that was clicked
   for (let l = 0; l < $(".glass").length; l++) {
-      let o = $(".glass")[l];
-      o !== t && move(o), o === t && (o.style.transform = "translateX(0px) translateY(0px)", o.style.zIndex = '2', o.style.opacity = "1")
+      let o = $(".glass")[l]; // all of the cards
+      o !== t && move(o), o === t && // if the card selected in the for loop wasn't the card clicked, focus card clicked
+      (o.style.transform = "translateX(0px) translateY(0px)", 
+      o.style.zIndex = '2', 
+      o.style.cursor = 'default',
+      o.style.opacity = "1")
   }
 }
-var bool = !1;
+var bool = true;// to stop both cards moving towards the same side
 
 function move(t) {
-  0 == bool ? (t.style.transform = "translateX(170px) translateY(-5px)", t.style.opacity = "0.3", t.style.zIndex = '1', bool = !0) : (t.style.transform = "translateX(-170px) translateY(5px)", t.style.opacity = "0.3", t.style.zIndex = '1', bool = !1)
+  false == bool ? // if toggle boolean is false, move card right:
+  
+  (t.style.transform = "translateX(170px) translateY(-5px)", 
+  t.style.opacity = "0.3", 
+  t.style.cursor = 'w-resize',
+  t.style.zIndex = '1', 
+  bool = true) 
+
+  : // if toggle boolean is true, move card left:
+
+  (t.style.transform = "translateX(-170px) translateY(5px)", 
+  t.style.cursor = 'e-resize',
+  t.style.opacity = "0.3", 
+  t.style.zIndex = '1', 
+  bool = false) // reset toggle boolean
 }
 
 function getindex(t) {
   for (var l = 0; null != (t = t.previousSibling);) l++;
   return Math.floor(l / 2)
 }
+
+
+function chooseColor() {
+  return backgrounds[Math.floor(Math.random() * backgrounds.length)]
+}
+document.body.style.background = chooseColor()
