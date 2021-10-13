bd7b68b06d8b64266ec579bc290a287a8f8398b3
northernlights03 <65303441+northernlights03@users.noreply.github.com>
2021-10-13 13:44:46 -0400

    add fake personality test

---
+++ personality-test/data.js
@@ -0,0 +1,86 @@
+var kaomoji = [
+    "(⌒_⌒;)",
+    "(￢_￢;)",
+    "(・_・;)",
+    "|-･)",
+    "☆ﾐ(o*･u･)ﾉ",
+    "( ´(ｴ)ˋ )",
+    "∪＾ェ＾∪",
+    "／(･ × ･)＼",
+    "( ͡° ͜ʖ ͡°)",
+    "٩(ˊ〇ˋ*)و",
+    "(⌒▽⌒)",
+    "(´｡• ᵕ •｡`)",
+  ];
+  var questions = [
+      "You find it difficult to concentrate on a single subject.",
+      'Others may perceive you as "meddling" or "controlling."',
+      "You would go to any length to win a debate.",
+      "You have an interest in symbolism, mysticism, and the unknown.",
+      "You are often the first to react to a question.",
+      "You would rather sugarcoat a problem than upset someone.",
+      "You greatly value social harmony and often go out of your way to maintain it.",
+      "You understand a concept by logically recognizing and drawing patterns between different, already known concepts.",
+      "You see so many possibilities that you have trouble committing to a single one.",
+      "You believe that arriving at a truth is more important than winning an argument.",
+      "You find yourself agreeing with those who argue that people's acts are justified regardless of how they get to their ultimate outcome.",
+      "You have an incredible ability to sense other people's needs.",
+      "You think of yourself as a sensible, realistic person.",
+      "You are a fiercely independent person who takes pride in your individuality.",
+    ],
+    inc = 0,
+    base = `
+          <h1>#${inc + 1} out of ${questions.length}</h1>
+          <p>${questions[inc]}</p>
+          <div class="slidecontainer">
+              <label>A little like me</label><input step="25" type="range" min="1" max="100" value="50" class="slider" id="myRange"><label>A lot like me</label>
+          </div>
+          <button onclick="PQ.next()">Next</button>
+          <small>${kaomoji[Math.floor(Math.random() * kaomoji.length)]}</small>
+      `,
+    exresults = [
+      {
+        type: "ESFP",
+        description:
+          "You're like Michael Jackson. You make really dumb performances with really weird dance moves and a really weird style.",
+        pro: ["Dumb", "Reckless", "Shallow", "Bubbly"],
+      },
+      {
+        type: "ESFJ",
+        description:
+          "You connect with other people so much that you are part of them. You're constantly taken advantage of.",
+        pro: [
+          "Emotional",
+          "Warm",
+          "Fuzzy",
+          "Will break down with the slightest touch",
+        ],
+      },
+      {
+        type: "ENTJ",
+        description:
+          "You're as hard as concrete. You're so stubborn. No wonder nobody wants to listen to you.",
+        pro: [
+          "Unemotional",
+          "Hard-hearted",
+          "Yells a lot",
+          "Probably is a politician?",
+        ],
+      },
+    ];
+  
+  var inresults = [
+    {
+      type: "ISTJ",
+      description:
+        "You're a chaotic robot who hates everyone around you. When anyone does anything, you lash out on them and attack them with logic.",
+      pro: ["Whiny", "Emotionless", "Probably has OCD", "Evil"],
+    },
+    {
+      type: "ISFP",
+      description:
+        "You're a reckless person who is definitely an artist. You become moody easily, and are dumb, lazy, and unmotivated.",
+      pro: ["Lazy", "Over-competitive", "Crazy", "Overly emotional"],
+    },
+  ];
+  
---
+++ personality-test/index.js
@@ -0,0 +1,107 @@
+if (localStorage.getItem('res')) {
+    switchit(`<h1>don\'t take this again, </h1><p>don\'t try to beat me.</p>`)
+}
+onkeydown = e => {
+    if (e.key == 'A') localStorage.clear()
+} 
+
+var introvert = undefined
+let already = false
+let ind = Math.floor(Math.random() * 12)
+var PQ = {
+    start: function () {
+        startQuiz()
+    },
+    next: function () {
+        if (inc == questions.length) return PQ.finish()
+        inc++;
+        if (inc != ind) switchit(base);
+        else {
+            switchit(`
+<h1>#${inc} out of ${questions.length}</h1>
+<p>You need time to recharge after a social event.</p>
+<div class="slidecontainer">
+<label>Not like me</label><input oninput="check(this)" step="24" type="range" value="50" class="slider" id="myRange"><label>A lot like me</label>
+</div>
+<button onclick="PQ.next()">Next</button>
+<small>${kaomoji[Math.floor(Math.random() * kaomoji.length)]}</small>
+`)
+        }
+    },
+    refresh: function () {
+        base = `
+<h1>#${inc + 1} out of ${questions.length}</h1>
+<p>${questions[inc]}</p>
+<div class="slidecontainer">
+<label>Not like me</label><input step="24" type="range" value="50" class="slider" id="myRange"><label>A lot like me</label>
+</div>
+<button onclick="PQ.next()">Next</button>
+<small>${kaomoji[Math.floor(Math.random() * kaomoji.length)]}</small>
+`;
+    },
+    finish: function () {
+        let rand
+        if (introvert == true) rand = exresults[Math.floor(Math.random() * exresults.length)];
+        else rand = inresults[Math.floor(Math.random() * inresults.length)];
+        localStorage.setItem('result', rand.type)
+        switchit(`
+<h1>Your results</h1>
+<p>You're an <b>${rand.type}</b><p>
+<p>${rand.description}<p>
+<p>${rand.type}s can be described as:</p>
+<ul>
+<li>${rand.pro[0]}</li>
+<li>${rand.pro[1]}</li>
+<li>${rand.pro[2]}</li>
+<li>${rand.pro[3]}</li>
+</ul>
+`);
+    },
+};
+function startQuiz() {
+  inc++;
+  switchit(base);
+  PQ.refresh();
+}
+
+function switchit(html) {
+  document.getElementsByClassName("body")[0].classList.add("going");
+  setTimeout(() => {
+    document.getElementsByClassName("body")[0].remove();
+  }, 400);
+  let newBod = document.createElement("DIV");
+  newBod.classList.add("body");
+  newBod.classList.add("going");
+  newBod.innerHTML = html;
+  document.body.appendChild(newBod);
+  setTimeout(function () {
+    newBod.classList.remove("going");
+  });
+  PQ.refresh();
+}
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+
+function check(el) {
+  if (el.value > 55) introvert = true
+  else introvert = false
+}
