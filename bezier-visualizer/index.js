ed0d8239c0d03b5a0fa59ae1171506a2cf7863ba
PineappleRind <pineapplerind.info@gmail.com>
2021-10-14 16:10:50 -0400

    FINALLY YOU CAN DRAG POINTS

--- bezier-visualizer/index.js
+++ bezier-visualizer/index.js
@@ -1,24 +1,43 @@
 function lerp(start, end, amt) {
   return (1 - amt) * start + amt * end;
 }
-function easeInOutCubic(x) {
-  return x < 0.5 ? 16 * x * x * x * x * x : 1 - Math.pow(-2 * x + 2, 5) / 2;
+function quadraticEaseInOut(x) {
+  return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
+}
+function exponentialEaseInOut(x) {
+  return x === 0
+    ? 0
+    : x === 1
+      ? 1
+      : x < 0.5 ? Math.pow(2, 20 * x - 10) / 2
+        : (2 - Math.pow(2, -20 * x + 10)) / 2;
+}
+function linear(x) {
+  return x
 }
 if (localStorage.getItem('bezierBannerState')) document.querySelector('.banner').remove()
-var firstPoint = [150, 490];
-var secondPoint = [325, 500];
-var thirdPoint = [450, 60];
-var fourthPoint = [50, 60];
+var points = [
+  [731, 551],
+  [338, 551],
+  [731, 214],
+  [338, 214]
+]
 
+var playing = true
 let canv = document.getElementById("canvas"),
   ctx = canv.getContext("2d"),
   canv2 = document.getElementById("canvas2"),
   ctx2 = canv2.getContext("2d"),
   background = "#000020",
-  t = 0;
+  t = 0, easedT, inter;
+
 
 (canv.height = window.innerHeight), (canv.width = window.innerWidth);
 (canv2.height = window.innerHeight), (canv2.width = window.innerWidth);
+onresize = () => {
+  (canv.height = window.innerHeight), (canv.width = window.innerWidth);
+  (canv2.height = window.innerHeight), (canv2.width = window.innerWidth);
+}
 class Point {
   constructor(x, y, radius) {
     this.x = x;
@@ -28,12 +47,13 @@ class Point {
 }
 
 function point(x, y, rad, col) {
-  if (col) ctx.fillStyle = col
-  else ctx.fillStyle = "#ffffff";
+
   ctx.beginPath();
 
   ctx.arc(x, y, rad, 0, 2 * Math.PI, true);
   ctx.closePath();
+  if (!col) ctx.fillStyle = "#ffffff";
+  else ctx.fillStyle = col
   ctx.fill();
 }
 function line(startx, starty, finishx, finishy) {
@@ -43,43 +63,59 @@ function line(startx, starty, finishx, finishy) {
   ctx.lineTo(finishx, finishy);
   ctx.stroke(); // Bottom side line
 }
-function advance() {
+function stp() {
+  clearInterval(inter);
+  if (t >= 1) t = 0, playing = false, evaluatePlaying()
+}
+strt(true)
+function strt(refresh) {
+  clearCanvas()
+  if (!refresh) clearTrail()
+  inter = setInterval(function () {
+    advance(exponentialEaseInOut);
+  }, 10);
 
+}
+function evaluatePlaying() {
+  if (playing == true) strt(true), document.getElementById('playBtn').innerHTML = 'Stop';
+  else stp(), document.getElementById('playBtn').innerHTML = 'Play';
+}
+function advance(ease) {
   clearCanvas();
-  if (t >= 1) clearInterval(inter);
+  if (t >= 1) stp();
   t = t + 0.0015;
-  easedT = easeInOutCubic(t);
+  easedT = ease(t);
 
-  new Point(firstPoint[0], firstPoint[1], 10, 'coral'); // Anchor dot
-  new Point(secondPoint[0], secondPoint[1], 10, 'magenta'); // Right side anchor dot
-  new Point(thirdPoint[0], thirdPoint[1], 10, 'dodgerblue'); // bottom side anchor dot
-  new Point(fourthPoint[0], fourthPoint[1], 10, 'mint'); // bottom right side anchor dot
+  new Point(points[0][0], points[0][1], 10, 'coral'); // Anchor dot
+  new Point(points[1][0], points[1][1], 10, 'magenta'); // Right side anchor dot
+  new Point(points[2][0], points[2][1], 10, 'dodgerblue'); // bottom side anchor dot
+  new Point(points[3][0], points[3][1], 10, 'mint'); // bottom right side anchor dot
 
-  line(thirdPoint[0], thirdPoint[1], fourthPoint[0], fourthPoint[1]);
-  line(firstPoint[0], firstPoint[1], thirdPoint[0], thirdPoint[1]);
-  line(secondPoint[0], secondPoint[1], fourthPoint[0], fourthPoint[1]);
+  line(points[2][0], points[2][1], points[3][0], points[3][1]);
+  line(points[0][0], points[0][1], points[2][0], points[2][1]);
+  line(points[1][0], points[1][1], points[3][0], points[3][1]);
 
   let topMiddlePointX = lerp(
-    firstPoint[0], thirdPoint[0], easedT
+    points[0][0], points[2][0], easedT
   )
   let topMiddlePointY = lerp(
-    firstPoint[1], thirdPoint[1], easedT
+    points[0][1], points[2][1], easedT
   )
   new Point(topMiddlePointX, topMiddlePointY, 5)
 
   let middleBottomPointX = lerp(
-    thirdPoint[0], fourthPoint[0], easedT
+    points[2][0], points[3][0], easedT
   )
   let middleBottomPointY = lerp(
-    thirdPoint[1], fourthPoint[1], easedT
+    points[2][1], points[3][1], easedT
   )
   new Point(middleBottomPointX, middleBottomPointY, 5)
 
   let bottomRightPointX = lerp(
-    fourthPoint[0], secondPoint[0], easedT
+    points[3][0], points[1][0], easedT
   )
   let bottomRightPointY = lerp(
-    fourthPoint[1], secondPoint[1], easedT
+    points[3][1], points[1][1], easedT
   )
   new Point(bottomRightPointX, bottomRightPointY, 5)
 
@@ -133,9 +169,53 @@ function advance() {
 }
 
 function clearCanvas() {
-  ctx.fillStyle = "#000020";
-  ctx.fillRect(0, 0, canv.width, canv.height);
+  ctx.clearRect(0, 0, canv.width, canv.height);
+
 }
-let inter = setInterval(function () {
-  advance();
-}, 10);
+function clearTrail() {
+  ctx2.clearRect(0, 0, canv.width, canv.height);
+}
+
+
+let mouseIsDown = false
+let draggingPoint = false
+onmousedown = () => {
+  mouseIsDown = true
+  console.log(mouseIsDown)
+}
+
+onmouseup = () => {
+  mouseIsDown = false
+  dragging = -1
+  console.log(mouseIsDown)
+}
+
+onmousemove = windowEvent => {
+  if (mouseIsDown == true) {
+    var x = windowEvent.clientX
+    var y = windowEvent.clientY
+    for (let i = 0; i < points.length; i++) {
+      if (
+        (
+          (
+            (x >= points[i][0] && x <= points[i][0] + 30) || (x <= points[i][0] && x >= points[i][0] - 30))
+            &&
+            ((y >= points[i][1] && y <= points[i][1] + 30) || (y <= points[i][1] && y >= points[i][1] - 30))
+          ) || dragging == i) {
+        dragging = i
+        points[i][0] = x
+        points[i][1] = y
+        clearCanvas();
+        new Point(points[0][0], points[0][1], 10, 'coral'); // Anchor dot
+        new Point(points[1][0], points[1][1], 10, 'magenta'); // Right side anchor dot
+        new Point(points[2][0], points[2][1], 10, 'dodgerblue'); // bottom side anchor dot
+        new Point(points[3][0], points[3][1], 10, 'mint'); // bottom right side anchor dot
+
+        line(points[2][0], points[2][1], points[3][0], points[3][1]);
+        line(points[0][0], points[0][1], points[2][0], points[2][1]);
+        line(points[1][0], points[1][1], points[3][0], points[3][1]);
+        break;
+      }
+    }
+  }
+}
