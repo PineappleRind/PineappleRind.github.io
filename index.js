b9f8bb4f4b2e2c71157fb19a807f13ccbc477a95
PineappleRind <pineapplerind.info@gmail.com>
2021-10-14 09:46:22 -0400

    Add bezier visualizer

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-var projects = [
+const projects = [
     {
         name: "Memelist.ml",
         description: "Funny concepts that you can say or refer to sometimes that will not make sense to most people.",
         img: 'imgs/memelist.png',
-        link: 'https://memelist.ml',
-        source: false
+        link: 'https://memelist.ml', // Memelist
     },
     {
         name: "bigsur-modal",
-        description: "4kb minified library to make alert dialogs in the style of MacOS Big Sur.",
+        description: "2.4kb minified library to make alert dialogs in the style of MacOS Big Sur.",
         img: 'imgs/bigsur-modal.png',
-        link: 'https://pineapplerind.ga/libraries/bigsur-modal',
-        source: 'https://github.com/pineapplerind/bigsur-modal'
+        link: 'https://pineapplerind.ga/libraries/bigsur-modal'
+    },
+    {
+        name: "Radium (Soon)",
+        description: "A fresh-looking theme for VSCode. Almost radioactive.",
+        img: 'imgs/comingsoon.png',
+        link: false
     }
 ]
 let base = `<div id="body">        <div class="section">
 <h1>pineapplerind</h1>
-<button onclick="toProjects(page)">projects &rarr;</button>
+<button onclick="toProjects(page)">projects</button>
 </div>
 
 </div>`
@@ -25,29 +29,20 @@ function newt(url) {
     window.open(url, '_blank').focus();
 }
 for (let i = 0; i < projects.length; i++) {
-   if (!projects[i].source) {
-    page += `<div class="sectionofboxes">
-    <div class="box">
-    <img src="${projects[i].img}">
-    <h1>${projects[i].name}</h1>
-    <p>${projects[i].description}</p>
-    <div class="box-buttons">
-        <button onclick="newt('${projects[i].link}')">Link</button>
-        <button disabled>Private source</button>
-        </div>
-    </div></div>`
-   } else {
-    page += `<div class="sectionofboxes">
-    <div class="box">
-    <img src="${projects[i].img}">
-    <h1>${projects[i].name}</h1>
-    <p>${projects[i].description}</p>
-    <div class="box-buttons">
-        <button onclick="newt('${projects[i].link}')">Link</button>
-        <button onclick="newt('${projects[i].source}')">Source</button>
-        </div>
-    </div></div>`
+    let link = `<button onclick="newt('${projects[i].link}')">Link</button>`
+   if (!projects[i].link) {
+       link = `<button disabled>No website</button>`
    }
+   page += `<div class="sectionofboxes">
+    <div class="box">
+    <img src="${projects[i].img}">
+    <div><h1>${projects[i].name}</h1>
+    <p>${projects[i].description}</p>
+    <div class="box-buttons">
+        ${link}
+        </div>
+    </div></div></div>`
+
     if (i == projects.length - 1) {
         page += `<button style="position:fixed;top:20px;right:20px;" onclick="toProjects(base)">Go back</button>`
     }
