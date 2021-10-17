dd97cfc01657ffe77763bacebb37f72d0af4d2a1
northernlights03 <65303441+northernlights03@users.noreply.github.com>
2021-10-17 16:48:35 -0400

    add new method but comment out bc not ready

--- bezier-visualizer/index.js
+++ bezier-visualizer/index.js
@@ -1,6 +1,5 @@
-
 function lerp(start, end, amt) {
-  return (1 - amt) * start + amt * end;
+  return Math.round(((1 - amt) * start + amt * end)*100)/100;
 }
 function quadraticEaseInOut(x) {
   return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
@@ -9,46 +8,53 @@ function exponentialEaseInOut(x) {
   return x === 0
     ? 0
     : x === 1
-      ? 1
-      : x < 0.5 ? Math.pow(2, 20 * x - 10) / 2
-        : (2 - Math.pow(2, -20 * x + 10)) / 2;
+    ? 1
+    : x < 0.5
+    ? Math.pow(2, 20 * x - 10) / 2
+    : (2 - Math.pow(2, -20 * x + 10)) / 2;
 }
 function linear(x) {
-  return x
+  return x;
 }
 var points = {
-  "data": [
+  data: [
     [531, 351],
     [138, 351],
     [531, 14],
-    [138, 14]
-  ]
-}
+    [138, 14],
+  ],
+
+  computed: [],
+};
+
 var save = {
   get: function () {
-    return JSON.parse(localStorage.getItem('bezierSaveData'))
+    return JSON.parse(localStorage.getItem("bezierSaveData"));
   },
   set: function () {
-    return localStorage.setItem('bezierSaveData', JSON.stringify(points))
-  }
-}
+    return localStorage.setItem("bezierSaveData", JSON.stringify(points));
+  },
+};
 
-if (localStorage.getItem('bezierBannerState')) document.querySelector('.banner').remove()
+if (localStorage.getItem("bezierBannerState"))
+  document.querySelector(".banner").remove();
 
 if (!save.get()) {
-  save.set()
+  save.set();
 } else {
-  points = save.get()
+  points = save.get();
 }
-var playing = true
+var playing = true;
 let canv = document.getElementById("canvas"),
   ctx = canv.getContext("2d"),
   canv2 = document.getElementById("canvas2"),
   ctx2 = canv2.getContext("2d"),
   background = "#000020",
-  t = 0, speed = 0.002, easedT, inter,
-  easeSelected = window['quadraticEaseInOut'];
-
+  t = 0,
+  speed = 0.002,
+  easedT,
+  inter,
+  easeSelected = window["quadraticEaseInOut"];
 
 (canv.height = window.innerHeight), (canv.width = window.innerWidth);
 (canv2.height = window.innerHeight), (canv2.width = window.innerWidth);
@@ -57,7 +63,7 @@ onresize = () => {
   canv.width = window.innerWidth;
   (canv2.height = window.innerHeight), (canv2.width = window.innerWidth);
   advance(easeSelected);
-}
+};
 class Point {
   constructor(x, y, radius) {
     this.x = x;
@@ -67,13 +73,12 @@ class Point {
 }
 
 function point(x, y, rad, col) {
-
   ctx.beginPath();
 
   ctx.arc(x, y, rad, 0, 2 * Math.PI, true);
   ctx.closePath();
   if (!col) ctx.fillStyle = "#ffffff";
-  else ctx.fillStyle = col
+  else ctx.fillStyle = col;
   ctx.fill();
 }
 function line(startx, starty, finishx, finishy) {
@@ -85,34 +90,33 @@ function line(startx, starty, finishx, finishy) {
 }
 function stp(y) {
   clearInterval(inter);
-  if (t >= 1 || y == true) t = 0, playing = false, evaluatePlaying()
+  if (t >= 1 || y == true) (t = 0), (playing = false), evaluatePlaying();
 }
-strt(true)
+strt(true);
 function strt(refresh) {
-
-  clearCanvas()
-  if (!refresh) clearTrail(), stp(true)
+  clearCanvas();
+  if (!refresh) clearTrail(), stp(true);
   inter = setInterval(function () {
     advance(easeSelected);
   }, 10);
-
 }
 function evaluatePlaying() {
-  if (playing == true) strt(true), document.getElementById('playBtn').innerHTML = 'Stop';
-  else stp(), document.getElementById('playBtn').innerHTML = 'Play';
+  if (playing == true)
+    strt(true), (document.getElementById("playBtn").innerHTML = "Stop");
+  else stp(), (document.getElementById("playBtn").innerHTML = "Play");
 }
 function replay() {
-  clearInterval(inter)
-  t = 1
-  strt(), document.getElementById('playBtn').innerHTML = 'Stop';
-  playing = true
+  clearInterval(inter);
+  t = 1;
+  strt(), (document.getElementById("playBtn").innerHTML = "Stop");
+  playing = true;
 }
 function advance(ease) {
   clearCanvas();
+  document.getElementById('speedometer').innerHTML = 't='+ease(t).toFixed(4)
   if (t >= 1) stp();
-  t += speed
+  t += speed;
   easedT = ease(t);
-
   new Point(points.data[0][0], points.data[0][1], 10, 'coral'); // Anchor dot
   new Point(points.data[1][0], points.data[1][1], 10, 'magenta'); // Right side anchor dot
   new Point(points.data[2][0], points.data[2][1], 10, 'dodgerblue'); // bottom side anchor dot
@@ -121,157 +125,170 @@ function advance(ease) {
   line(points.data[2][0], points.data[2][1], points.data[3][0], points.data[3][1]);
   line(points.data[0][0], points.data[0][1], points.data[2][0], points.data[2][1]);
   line(points.data[1][0], points.data[1][1], points.data[3][0], points.data[3][1]);
+ // drawAndConnectInitialPoints();
+  //getFirstMidPoints();
+  //drawMidPoints();
+   let topMiddlePointX = lerp(points.data[0][0], points.data[2][0], easedT);
+  let topMiddlePointY = lerp(points.data[0][1], points.data[2][1], easedT);
+  new Point(topMiddlePointX, topMiddlePointY, 5);
 
-  let topMiddlePointX = lerp(
-    points.data[0][0], points.data[2][0], easedT
-  )
-  let topMiddlePointY = lerp(
-    points.data[0][1], points.data[2][1], easedT
-  )
-  new Point(topMiddlePointX, topMiddlePointY, 5)
+  let middleBottomPointX = lerp(points.data[2][0], points.data[3][0], easedT);
+  let middleBottomPointY = lerp(points.data[2][1], points.data[3][1], easedT);
+  new Point(middleBottomPointX, middleBottomPointY, 5);
 
-  let middleBottomPointX = lerp(
-    points.data[2][0], points.data[3][0], easedT
-  )
-  let middleBottomPointY = lerp(
-    points.data[2][1], points.data[3][1], easedT
-  )
-  new Point(middleBottomPointX, middleBottomPointY, 5)
-
-  let bottomRightPointX = lerp(
-    points.data[3][0], points.data[1][0], easedT
-  )
-  let bottomRightPointY = lerp(
-    points.data[3][1], points.data[1][1], easedT
-  )
-  new Point(bottomRightPointX, bottomRightPointY, 5)
+  let bottomRightPointX = lerp(points.data[3][0], points.data[1][0], easedT);
+  let bottomRightPointY = lerp(points.data[3][1], points.data[1][1], easedT);
+  new Point(bottomRightPointX, bottomRightPointY, 5);
 
   line(
-    topMiddlePointX, topMiddlePointY,
-    middleBottomPointX, middleBottomPointY
-  )
+    topMiddlePointX,
+    topMiddlePointY,
+    middleBottomPointX,
+    middleBottomPointY
+  );
   line(
-    middleBottomPointX, middleBottomPointY,
-    bottomRightPointX, bottomRightPointY
-  )
+    middleBottomPointX,
+    middleBottomPointY,
+    bottomRightPointX,
+    bottomRightPointY
+  );
 
-  let midpoint1X = lerp(
-    topMiddlePointX, middleBottomPointX, easedT
-  )
-  let midpoint1Y = lerp(
-    topMiddlePointY, middleBottomPointY, easedT
-  )
-  new Point(midpoint1X, midpoint1Y, 5)
+  let midpoint1X = lerp(topMiddlePointX, middleBottomPointX, easedT);
+  let midpoint1Y = lerp(topMiddlePointY, middleBottomPointY, easedT);
+  new Point(midpoint1X, midpoint1Y, 5);
 
-  let midpoint2X = lerp(
-    middleBottomPointX, bottomRightPointX, easedT
-  )
-  let midpoint2Y = lerp(
-    middleBottomPointY, bottomRightPointY, easedT
-  )
-  new Point(midpoint2X, midpoint2Y, 5)
-  line(
-    midpoint1X, midpoint1Y,
-    midpoint2X, midpoint2Y
-  )
-  let finalMidPointX = lerp(
-    midpoint1X,
-    midpoint2X,
-    easedT
-  )
-  let finalMidPointY = lerp(
-    midpoint1Y,
-    midpoint2Y,
-    easedT
-  )
-  new Point(finalMidPointX, finalMidPointY, 10)
+  let midpoint2X = lerp(middleBottomPointX, bottomRightPointX, easedT);
+  let midpoint2Y = lerp(middleBottomPointY, bottomRightPointY, easedT);
+  new Point(midpoint2X, midpoint2Y, 5);
+  line(midpoint1X, midpoint1Y, midpoint2X, midpoint2Y);
+  let finalMidPointX = lerp(midpoint1X, midpoint2X, easedT);
+  let finalMidPointY = lerp(midpoint1Y, midpoint2Y, easedT);
+  new Point(finalMidPointX, finalMidPointY, 10);
   /*
   Trail
   */
   ctx2.beginPath();
   ctx2.fillStyle = "#ffffff";
-  ctx2.arc(finalMidPointX, finalMidPointY, 10, 0, 2 * Math.PI, true);
+ ctx2.arc(finalMidPointX, finalMidPointY, 10, 0, 2 * Math.PI, true);
   ctx2.closePath();
   ctx2.fill();
 }
 
 function clearCanvas() {
   ctx.clearRect(0, 0, canv.width, canv.height);
-
 }
 function clearTrail() {
   ctx2.clearRect(0, 0, canv.width, canv.height);
 }
 function resetCurve() {
   points = {
-    "data": [
+    data: [
       [531, 351],
       [138, 351],
       [531, 14],
-      [138, 14]
-    ]
-  }
-  save.set()
+      [138, 14],
+    ],
+  };
+  save.set();
 }
 
-let mouseIsDown = false
-let dragging = -1
+let mouseIsDown = false;
+let dragging = -1;
 onmousedown = () => {
-  mouseIsDown = true
-}
+  mouseIsDown = true;
+};
 ontouchstart = () => {
-  mouseIsDown = true
-  console.log(mouseIsDown)
-}
+  mouseIsDown = true;
+  console.log(mouseIsDown);
+};
 
 onmouseup = () => {
-  mouseIsDown = false
-  dragging = false
-}
-onmousemove = e => {
-  pointHandler(e)
-}
-ontouchmove = e => {
-  pointHandler(e.touches[0])
-}
+  mouseIsDown = false;
+  dragging = false;
+};
+onmousemove = (e) => {
+  pointHandler(e);
+};
+ontouchmove = (e) => {
+  pointHandler(e.touches[0]);
+};
 function pointHandler(windowEvent) {
   if (mouseIsDown == true) {
-    console.log(mouseIsDown)
-    var x = windowEvent.clientX
-    var y = windowEvent.clientY
+    console.log(mouseIsDown);
+    var x = windowEvent.clientX;
+    var y = windowEvent.clientY;
     for (let i = 0; i < points.data.length; i++) {
       if (
-        (
-          (
-            (x >= points.data[i][0] && x <= points.data[i][0] + 30) || (x <= points.data[i][0] && x >= points.data[i][0] - 30))
-          &&
-          ((y >= points.data[i][1] && y <= points.data[i][1] + 30) || (y <= points.data[i][1] && y >= points.data[i][1] - 30))
-        )) {
-        dragging = i
-        if (x < 0) break;
-        if (y < 0) break;
-        if (x > window.innerWidth) break;
-        if (y > window.innerHeight) break;
-        points.data[i][0] = x
-        points.data[i][1] = y
-        clearCanvas();
-        new Point(points.data[0][0], points.data[0][1], 10, '#000000'); // Anchor dot
-        new Point(points.data[1][0], points.data[1][1], 10, 'magenta'); // Right side anchor dot
-        new Point(points.data[2][0], points.data[2][1], 10, 'dodgerblue'); // bottom side anchor dot
-        new Point(points.data[3][0], points.data[3][1], 10, 'mint'); // bottom right side anchor dot
+        ((x >= points.data[i][0] && x <= points.data[i][0] + 30) ||
+          (x <= points.data[i][0] && x >= points.data[i][0] - 30)) &&
+        ((y >= points.data[i][1] && y <= points.data[i][1] + 30) ||
+          (y <= points.data[i][1] && y >= points.data[i][1] - 30))
+      ) {
+        dragging = i;
+        if (dragging == i) {
+          if (x < 0) break;
+          if (y < 0) break;
+          if (x > window.innerWidth) break;
+          if (y > window.innerHeight) break;
+          points.data[i][0] = x;
+          points.data[i][1] = y;
+          clearCanvas();
+          new Point(points.data[0][0], points.data[0][1], 10, 'coral'); // Anchor dot
+          new Point(points.data[1][0], points.data[1][1], 10, 'magenta'); // Right side anchor dot
+          new Point(points.data[2][0], points.data[2][1], 10, 'dodgerblue'); // bottom side anchor dot
+          new Point(points.data[3][0], points.data[3][1], 10, 'mint'); // bottom right side anchor dot
+        
+          line(points.data[2][0], points.data[2][1], points.data[3][0], points.data[3][1]);
+          line(points.data[0][0], points.data[0][1], points.data[2][0], points.data[2][1]);
+          line(points.data[1][0], points.data[1][1], points.data[3][0], points.data[3][1]);
 
-        line(points.data[2][0], points.data[2][1], points.data[3][0], points.data[3][1]);
-        line(points.data[0][0], points.data[0][1], points.data[2][0], points.data[2][1]);
-        line(points.data[1][0], points.data[1][1], points.data[3][0], points.data[3][1]);
+          save.set();
 
-        save.set()
-
-        break;
+          break;
+        }
       }
     }
   }
 }
-
-document.getElementById('easeOption').oninput = () => {
-  easeSelected = window[document.getElementById('easeOption').value]
+function drawAndConnectInitialPoints() {
+  for (let i = 0; i < points.data.length; i++) {
+    new Point(points.data[i][0], points.data[i][1], 10, "white"); // Anchor dots
+  }
+  for (let i = 0; i < points.data.length - 1; i++) {
+    line(
+      points.data[i][0],
+      points.data[i][1],
+      points.data[i + 1][0],
+      points.data[i + 1][1]
+    );
+  }
 }
+function getFirstMidPoints() {
+  for (let i = 0; i < points.data.length; i++) {
+    if (i == points.data.length - 1) {
+      break
+    }
+    points.computed[i][0] = lerp(
+      points.data[i][0],
+      points.data[i + 1][0],
+      easedT
+    );
+    points.computed[i][1] = lerp(
+      points.data[i][1],
+      points.data[i + 1][1],
+      easedT
+    );
+  }
+}
+function drawMidPoints() {
+  for (let i = 0; i < points.computed.length; i++) {
+    new Point(points.computed[i][0],points.computed[i][1],5)
+      if (i != points.computed.length - 1) line(
+        points.computed[i + 1][0], points.computed[i + 1][1],
+        points.computed[i][0], points.computed[i][1]
+      )
+  }
+}
+document.getElementById("easeOption").oninput = () => {
+  easeSelected = window[document.getElementById("easeOption").value];
+};
