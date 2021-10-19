034f24cd770d2b871024b266c27b0fd2bf6039b4
agn <pineapplerind.info@gmail.com>
2021-10-19 12:08:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ for (let i = 0; i < projects.length; i++) {
 }
 document.querySelector('.projects').innerHTML += page
 
-if (Math.random() < 0.8 || localStorage.getItem('pineapplerindSurveyState')) {
+if (Math.random() < 0.5 || localStorage.getItem('pineapplerindSurveyState')) {
     document.querySelector('form').style.display = 'none'
 }
