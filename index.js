e24b90ec973df37defd1cd2209b5ddbd519ae6ff
agn <pineapplerind.info@gmail.com>
2022-01-22 14:07:44 -0500

    text stats bug fix + homepage updates

--- index.js
+++ index.js
@@ -6,29 +6,11 @@ const projects = [
         link: "https://pineapplerind.ga/bezier-visualizer"
     },
     {
-        name: "bigsur-modal",
-        description: "2.4kb minified library to make alert dialogs in the style of MacOS Big Sur.",
-        img: 'imgs/bigsur-modal.png',
-        link: 'https://pineapplerind.ga/libraries/bigsur-modal'
-    },
-    {
-        name: "Color Theory Rants",
-        description: "Correcting a few misconceptions regarding colors.",
-        img: "imgs/color-theory-rants.png",
-        link: "https://pineapplerind.ga/color-theory-rants/"
-    },
-    {
         name: "Radium",
         description: "A fresh-looking theme for VSCode. Almost radioactive.",
         img: 'https://radium-theme.github.io/example.png',
         link: 'https://radium-theme.github.io'
     },
-    {
-        name: "Memelist.ml",
-        description: "Funny concepts that you can say or refer to sometimes that will not make sense to most people.",
-        img: 'imgs/memelist.png',
-        link: 'https://memelist.ml', // Memelist
-    }
 ]
 var page = ``
 function newt(url) {
--- text-stats/index.js
+++ text-stats/index.js
@@ -11,11 +11,14 @@ $('input').onkeyup = () => {
 function updateStats(input) {
 	$('words').innerHTML = input.words
 	$('letters').innerHTML = input.letters
-    $('sentences').innerHTML = input.sentences
+	$('sentences').innerHTML = input.sentences
 	$('letterswspaces').innerHTML = input.lettersWithoutSpaces
 	$('commonletters').innerHTML = ''
+
 	for (let i = 0; i < 8; i++) {
-		$('commonletters').innerHTML += `<b>${input.mostCommonLetters[i].name}</b> ${input.mostCommonLetters[i].score} (${Math.round((input.mostCommonLetters[i].score /input.letters * 100)*100)/100}%)<br>`
+		let letter = Object.keys(input.mostCommonLetters)[i]
+		let letterAmt = input.mostCommonLetters[Object.keys(input.mostCommonLetters)[i]]
+		$('commonletters').innerHTML += `<b>${letter}</b> ${letterAmt} (${Math.round((letterAmt / input.letters * 100) * 100) / 100}%)<br>`
 	}
 }
 
@@ -24,131 +27,38 @@ function parseInput(input) {
 	res.words = input.value.match(/\S+/g).length;
 	res.letters = input.value.length
 	res.lettersWithoutSpaces = input.value.match(/\S/g).length
-    res.sentences = input.value.match(/[^?!.][?!.]/g).length
-	res.mostCommonLetters = [{
-			name: 'a',
-			score: 0
-		},
-		{
-			name: 'b',
-			score: 0
-		},
-		{
-			name: 'c',
-			score: 0
-		},
-		{
-			name: 'd',
-			score: 0
-		},
-		{
-			name: 'e',
-			score: 0
-		},
-		{
-			name: 'f',
-			score: 0
-		},
-		{
-			name: 'g',
-			score: 0
-		},
-		{
-			name: 'h',
-			score: 0
-		},
-		{
-			name: 'i',
-			score: 0
-		},
-		{
-			name: 'j',
-			score: 0
-		},
-		{
-			name: 'k',
-			score: 0
-		},
-		{
-			name: 'l',
-			score: 0
-		},
-		{
-			name: 'm',
-			score: 0
-		},
-		{
-			name: 'n',
-			score: 0
-		},
-		{
-			name: 'o',
-			score: 0
-		},
-		{
-			name: 'p',
-			score: 0
-		},
-		{
-			name: 'q',
-			score: 0
-		},
-		{
-			name: 'r',
-			score: 0
-		},
-		{
-			name: 's',
-			score: 0
-		},
-		{
-			name: 't',
-			score: 0
-		},
-		{
-			name: 'u',
-			score: 0
-		},
-		{
-			name: 'v',
-			score: 0
-		},
-		{
-			name: 'w',
-			score: 0
-		},
-		{
-			name: 'x',
-			score: 0
-		},
-		{
-			name: 'y',
-			score: 0
-		},
-		{
-			name: 'z',
-			score: 0
-		},
-	]
-	console.log(`
-    words: ${res.words}
-    letters: ${res.letters}
-    letterswspaces: ${res.lettersWithoutSpaces}`)
+	let sentences = input.value.match(/[^?!.][?!.]/g)
+	if (!sentences) res.sentences = ''
+	else res.sentences = sentences.length
+	res.mostCommonLetters = {}
 	for (let i = 0; i < input.value.length; i++) {
-		for (let j = 0; j < res.mostCommonLetters.length; j++) {
-			if (input.value[i].toLowerCase() == res.mostCommonLetters[j].name) {
-				res.mostCommonLetters[j].score++
-			}
+		let cur = input.value[i]
+		if (parseInt(cur) < 10) continue
+		if (cur === ' ') continue
+		if (!res.mostCommonLetters[cur]) {
+			res.mostCommonLetters[cur] = 1;
+			continue
+		} else if (res.mostCommonLetters[cur]) {
+			res.mostCommonLetters[cur]++
+			continue
 		}
 	}
-	res.mostCommonLetters = sort(res.mostCommonLetters, 'score')
+
+	res.mostCommonLetters = sort(res.mostCommonLetters)
 	return res
 }
 
-function sort(array, key) {
-	return array.sort(function(a, b) {
-		var x = a[key];
-		var y = b[key];
-		return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+function sort(letters) {
+	var sortable = [];
+	for (var letter in letters) {
+		sortable.push([letter, letters[letter]]);
+	}
+	sortable.sort(function (a, b) {
+		return b[1] - a[1];
 	});
+	var objSorted = {}
+	sortable.forEach(function (item) {
+		objSorted[item[0]] = item[1]
+	})
+	return objSorted
 }
