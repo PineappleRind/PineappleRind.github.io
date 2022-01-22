fa913509a2935e12368ca56dd9b9749a1f8d6bf4
agn <pineapplerind.info@gmail.com>
2022-01-22 13:27:34 -0500

    restructuring & colours website

--- wiggle/index.js
+++ wiggle/index.js
@@ -40,4 +40,83 @@
         }
 
     }
+
+    function fallbackCopyTextToClipboard(text) {
+        var textArea = document.createElement("textarea");
+        textArea.value = text;
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+
+        try {
+            var successful = document.execCommand("copy");
+        } catch (err) {
+            console.error(err)
+        }
+
+        document.body.removeChild(textArea);
+    }
+
+    function copyTextToClipboard(text) {
+        if (!navigator.clipboard) {
+            fallbackCopyTextToClipboard(text);
+            return;
+        }
+        navigator.clipboard.writeText(text).then(
+            function() {
+                console.log("Async: Copying to clipboard was successful!");
+            },
+            function(err) {
+                console.error("Async: Could not copy text: ", err);
+            }
+        );
+    }
+    onclick = e => {
+        if (e.target.classList.value == 'output') {
+            console.log(e.target.classList)
+            console.log('OUCH')
+            copyTextToClipboard($('textarea').value)
+            let toast = new Toast({
+                title: 'Copied!',
+                description: 'Successfully copied to clipboard',
+                duration: 5
+            })
+            toast.initialize()
+            setTimeout(function() {
+                toast.show()
+            }, 1)
+        }
+    }
+    $('.output').onclick = () => {
+
+    }
+    let toastIsShowing = false
+
+    function Toast(obj) {
+        this.title = obj.title
+        this.description = obj.description
+        this.duration = obj.duration * 1000
+        this.initialize = function() {
+            toastIsShowing = true
+            let toastcont = $('.toastcont')
+            let toast = document.createElement('DIV')
+            toast.classList.add('toast')
+            toast.innerHTML = `<b>${this.title}</b>
+        <p>${this.description}</p>`
+            toastcont.appendChild(toast)
+            this.element = toast
+            return this
+        }
+        this.show = function() {
+            let toaststruct = this
+            setTimeout(function() { toaststruct.element.classList.add('showing') })
+            setTimeout(function() {
+                toaststruct.element.classList.remove('showing')
+                setTimeout(function() {
+                    toaststruct.element.remove()
+                }, 500)
+            }, this.duration)
+            return this
+        }
+    }
 })();
