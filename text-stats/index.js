8b9b83f1d968d172f143a69d20ef0fdc7287cdce
agn <pineapplerind.info@gmail.com>
2022-01-22 14:34:07 -0500

    text stats hotfixes

--- text-stats/index.js
+++ text-stats/index.js
@@ -18,6 +18,7 @@ function updateStats(input) {
 	for (let i = 0; i < 8; i++) {
 		let letter = Object.keys(input.mostCommonLetters)[i]
 		let letterAmt = input.mostCommonLetters[Object.keys(input.mostCommonLetters)[i]]
+		if (!letter) continue
 		$('commonletters').innerHTML += `<b>${letter}</b> ${letterAmt} (${Math.round((letterAmt / input.letters * 100) * 100) / 100}%)<br>`
 	}
 }
@@ -25,14 +26,14 @@ function updateStats(input) {
 function parseInput(input) {
 	let res = {}
 	res.words = input.value.match(/\S+/g).length;
-	res.letters = input.value.length
+	res.letters = input.value.replaceAll(' ','').length
 	res.lettersWithoutSpaces = input.value.match(/\S/g).length
 	let sentences = input.value.match(/[^?!.][?!.]/g)
 	if (!sentences) res.sentences = ''
 	else res.sentences = sentences.length
 	res.mostCommonLetters = {}
 	for (let i = 0; i < input.value.length; i++) {
-		let cur = input.value[i]
+		let cur = input.value[i].toLowerCase()
 		if (parseInt(cur) < 10) continue
 		if (cur === ' ') continue
 		if (!res.mostCommonLetters[cur]) {
@@ -56,9 +57,13 @@ function sort(letters) {
 	sortable.sort(function (a, b) {
 		return b[1] - a[1];
 	});
+	sortable.filter(letter=>{
+		letter != ' '
+	})
 	var objSorted = {}
 	sortable.forEach(function (item) {
 		objSorted[item[0]] = item[1]
 	})
+	console.log(sortable)
 	return objSorted
 }
