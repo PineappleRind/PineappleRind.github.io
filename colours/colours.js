f7a1f43b03e4a3c91247ce53f98293b0d2b7eb8c
agn <pineapplerind.info@gmail.com>
2022-01-25 11:06:11 -0500

    Update colours.js

--- colours/colours.js
+++ colours/colours.js
@@ -1,19 +1,27 @@
 function Ripple(obj) {
-	obj.element.style.overflow = 'hidden'
-    obj.element.style.position = "absolute"
-    obj.element.onclick = e => {
-    	let bounds = obj.element.parentElement.getBoundingClientRect();
-    	let x = e.clientX - bounds.left;
-    	let y = e.clientY - bounds.top;
-        let ripple = document.createElement('DIV')
-        ripple.classList.add('ripple')
-        ripple.style.left = x + 'px'
-        ripple.style.top= y+'px'
-        ripple.style.animationDuration = obj.duration + 's'
-        if (obj.color) ripple.style.background = obj.color
-        obj.element.appendChild(ripple)
-        setTimeout(function(){
-        	ripple.remove()
-        },obj.duration*1000)
+    if (obj.element.length > 0) {
+        for (let i = 0; i < obj.element.length; i++) {
+            __addRipple(obj, obj.element[i])
+        }
+    } else __addRipple(obj,obj.element)
+
+    function __addRipple(obj,el) {
+        el.style.overflow = 'hidden'
+        el.style.position = "relative"
+        el.onclick = e => {
+            let bounds = el.parentElement.getBoundingClientRect();
+            let x = e.clientX - bounds.left;
+            let y = e.clientY - bounds.top;
+            let ripple = document.createElement('DIV')
+            ripple.classList.add('ripple')
+            ripple.style.left = x + 'px'
+            ripple.style.top = y + 'px'
+            ripple.style.animationDuration = obj.duration + 's'
+            if (obj.color) ripple.style.background = obj.color
+            el.appendChild(ripple)
+            setTimeout(function() {
+                ripple.remove()
+            }, obj.duration * 1000)
+        }
     }
 }
