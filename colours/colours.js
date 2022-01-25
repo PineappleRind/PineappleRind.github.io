20f0ea4c5a3367fed4acdbeef7dcf29afa16adab
agn <pineapplerind.info@gmail.com>
2022-01-25 12:28:54 -0500

    finisch
    ye bruv innit

--- colours/colours.js
+++ colours/colours.js
@@ -3,13 +3,14 @@ function Ripple(obj) {
         for (let i = 0; i < obj.element.length; i++) {
             __addRipple(obj, obj.element[i])
         }
-    } else __addRipple(obj,obj.element)
+    } else __addRipple(obj, obj.element)
 
-    function __addRipple(obj,el) {
+    function __addRipple(obj, el) {
+        console.log(el)
         el.style.overflow = 'hidden'
         el.style.position = "relative"
         el.onclick = e => {
-            let bounds = el.parentElement.getBoundingClientRect();
+            let bounds = el.getBoundingClientRect();
             let x = e.clientX - bounds.left;
             let y = e.clientY - bounds.top;
             let ripple = document.createElement('DIV')
@@ -19,9 +20,29 @@ function Ripple(obj) {
             ripple.style.animationDuration = obj.duration + 's'
             if (obj.color) ripple.style.background = obj.color
             el.appendChild(ripple)
-            setTimeout(function() {
+            setTimeout(function () {
                 ripple.remove()
             }, obj.duration * 1000)
         }
     }
+}
+
+onload = () => {
+    let elementsArr = []
+    let cards = document.querySelectorAll('.card')
+    for (let i = 0; i < cards.length; i++) {
+        elementsArr.push(cards[i])
+        cards[i].onmouseup = () => {
+            let url = cards[i].getAttribute('redir')
+            setTimeout(function () {
+                window.open(url, '_blank').focus();
+            }, 0.5 * 1000)
+        }
+    }
+    document.querySelector('.colourscont').classList.add('showing-lol')
+    Ripple({
+        element: elementsArr,
+        duration: 0.8,
+        color: `rgba(255,255,255,0.3)`
+    })
 }
