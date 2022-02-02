ce8d689fcfbeafa2ef5aa4efc7a9289b27e27205
agn <pineapplerind.info@gmail.com>
2022-02-02 08:33:13 -0500

    Update index.js

--- flipanim-census/index.js
+++ flipanim-census/index.js
@@ -25,11 +25,13 @@ var Questions = [
 ]
 var form = document.getElementById('form'), // Form element 
     container = document.querySelector('.glass')//Container!
-    
+var thankYou = `
+        <h1>Thank you!</h1>
+        <p>Your answers have been submitted :D<br> The results will be announced when there are enough submissions, no more than a few weeks. I'll post an anim with the results when they are ready! Thank you for participating in this year's census ^^`
 var Census = { // I put this stuff inside an object just because it looks good, lol
     current: 0, // Current question
     question: function (obj) {
-        if (!obj) return Census.end() // If the question is nonexistent, end it all!
+        if (!obj) return Census.submit(Census.data) // If the question is nonexistent, end it all!
         Census.current++
         let html = `<h1>${obj.title}</h1><br><p>${obj.description}</p><br>${obj.actionItems}<br><button onclick="Census.question(Questions[${Census.current}])">Next</button>`
         Census.switch(html) // Switch the HTML of the page with the new, hot, freshly generated HTML!
@@ -41,27 +43,25 @@ var Census = { // I put this stuff inside an object just because it looks good, 
             container.innerHTML = html // Switch html...
             container.classList.remove('transitioning') // Tada! :D
         },500)
-    }, end: function() {
-        Census.submit(Census.data) // Submit the data!
-        
-
-        let html = `
-        <h1>Thank you!</h1>
-        <p>Your answers have been submitted :D<br> The results will be announced when there are enough submissions, no more than a few weeks. I'll post an anim with the results when they are ready! Thank you for participating in this year's census ^^`
-        Census.switch(html) // Destroy everything on the page, replacing it with the thank you message
     }, submit: function(data) {
+        
         // So i dont really know how to send the form other than 
         // making an invisible form and populating it
 
         // but it works... so... yeah
         // Put the users age in the invisible form:
-        form.children[0].value = data.age
+        form.children[0][0].value = data.age
         //Put gender...
-        form.children[1].value = data.gender
+        form.children[0][1].value = data.gender
         // And put follower count!
-        form.children[2].value = data.followers
-        // Finally send the form
-       form.submit()
+        form.children[0][2].value = data.followers
+        form.children[1][0].innerHTML = `<h1>Ready?</h1>
+<b>Age</b> ${data.age}
+<b>Gender</b> ${data.gender}
+<b>Followers</b> ${data.followers}
+<b>Comments</b> ${data.comments}
+<br>
+<button onclick="Census.switch(thankYou)" type="submit">Submit</button>`
     },
     recordData: function (key, value) {
        return Census.data[key] = value // Just records the data.. 
