405aef69210eb198c465b5a5627d2e4ea6c8fcaa
northernlights03 <65303441+northernlights03@users.noreply.github.com>
2022-04-27 16:02:44 -0400

    typescript

---
+++ new/data.js
@@ -0,0 +1,1 @@
+export const animations={"initial-header":{properties:{top:{values:[0,100],type:"px"},opacity:{values:[0,1],type:""}},element:document.querySelector("header"),duration:1,frame:0,ease:"easeInOutCubic"}};
