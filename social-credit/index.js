9c6b5be3633c5bdd177a66e20abf1dfaebbff3e9
pr <pineapplerind.info@gmail.com>
2023-04-05 10:23:22 -0400

    fix(social-credit): gif broken link

--- social-credit/index.js
+++ social-credit/index.js
@@ -1,3 +1,265 @@
-"use strict";var root="/social-credit/";function shuffle(a){let b=a.length,c;for(;0!=b;)c=Math.floor(Math.random()*b),b--,[a[b],a[c]]=[a[c],a[b]];return a}var questions=[{name:"what happened on May 20, 1989 in Tiananmen Square?",answers:[{name:"nothing",score:!0},{name:"something",score:!1}],result:"nothing happened on May 20, 1989 in Tiananmen Square"},{name:"what is Taiwan?",answers:[{name:"an independent country",score:!1},{name:"never heard of it",score:!0},{name:"a territory of china",score:!0}]},{name:"how long did you play video games this week?",answers:[{name:"6 hours or more",score:!1},{name:"4 hours",score:!1},{name:"2 hours or less",score:!0},{name:"i don't play video games",score:!0}]},{name:"which one is the glorious leader?",answers:[{name:"Xi Jinping",score:!0},{name:"Vladimir Putin",score:!1},{name:"Joe Biden",score:!1}]},{name:"what brand phone do you have?",answers:[{name:"apple",score:!1},{name:"huawei",score:!0},{name:"samsung",score:!1}],result:"chinese brand phones are best"},{name:`what do you think of this picture? <br> <img class="xi" src="${root}pooh.png">`,answers:[{name:"amusing",score:!1},{name:"unacceptable",score:!0},{name:"funny",score:!1},{name:"bing chilling",score:!1}]},{name:"how many kids do you have?",answers:[{name:"no kids",score:!0},{name:"1",score:!0},{name:"2",score:!0},{name:"more than 3",score:!1}]}];questions=shuffle(questions);const $=document.querySelectorAll.bind(document);let quesiter=0,container=document.querySelector(".container"),socCred=0,endScreen=function(){return socCred<300?(play(1),emoji(1),document.body.classList.add("ohnorun"),`
-        <h1>you have failed the test.</h1><p>your social credit score is ${socCred}. ${Math.random>.5?"Xi Jinping will personally order you to be banned from China. ":"you have been sentenced to a lifetime ban from china by our supreme Xi Jinping. "} you are not loyal enough to our glorious nation.`):(emoji(0,3),document.body.classList.add("passed"),`<h1>you have been proven to be a loyal citizen.</h1><p>your social credit score is ${socCred}. glory to the ccp!</p>`)};function play(a){$("audio")[+!a].pause(),$("audio")[a].play()}function nosound(){window.play=a=>null}function beginTest(){play(0),1!=questions.length?switchSlides(0):switchSlides(endScreen())}function switchSlides(a){container.classList.add("transitioning"),0===questions.length?setTimeout(function(){container.innerHTML=endScreen(),container.classList.remove("transitioning")},600):(quesiter++,setTimeout(function(){container.innerHTML=getQuestionHTML(a),container.classList.remove("transitioning")},600))}function getQuestionHTML(a){console.log(a);let c=`
-    <small>question ${quesiter}</small><br><h1>${questions[a].name}</h1><div class="buttons">`;for(let b=0;b<questions[a].answers.length;b++)c+=`<button onclick="answer(${questions[a].answers[b].score},${a})">${questions[a].answers[b].name}</button>`;return c+"</div>"}const emoji=(c,a)=>{let b=0,e=root+(0===c?"eye-china.png":"eye-pop.png");if(a)do d(b);while((b++)<a-1)function d(c){if(c>=a)return;let d=Math.floor(Math.random()*(window.innerWidth-300)),f=Math.floor(Math.random()*(window.innerHeight-300)),b=document.createElement("IMG");b.src=e,b.style.left=d+"px",b.style.top=f+"px",b.classList.add("chinese-emoji"),document.body.appendChild(b)}d()};function answer(a,c){let b=100+Math.round(50*Math.random());a||(b*=-1),emoji(+!a),socCred+=b,msg(b,a,questions[c].result),$(".counter")[0].innerHTML=`social credit: ${socCred}`,questions.shift()}function msg(c,b,d){let a=document.createElement("DIV");a.classList.add("toast"),a.innerHTML=`${!0===b?"+":""}${c} SOCIAL CREDIT <br><p>${d||""}</p><br><button onclick="switchSlides(0);purge(${b})"">NEXT</button>`,document.body.appendChild(a)}function purge(b){let a=$(".chinese-emoji");!1===b?a.forEach(a=>a.remove()):a[0].remove(),document.querySelector(".toast").remove()}
+var root = "./";
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+var questions = [
+  {
+    name: "what happened on May 20, 1989 in Tiananmen Square?",
+    answers: [
+      {
+        name: "nothing",
+        score: true,
+      },
+      {
+        name: "something",
+        score: false,
+      },
+    ],
+    result: "nothing happened on May 20, 1989 in Tiananmen Square",
+  },
+  {
+    name: "what is Taiwan?",
+    answers: [
+      {
+        name: "an independent country",
+        score: false,
+      },
+      {
+        name: "never heard of it",
+        score: true,
+      },
+      {
+        name: "a territory of china",
+        score: true,
+      },
+    ],
+  },
+  {
+    name: "how long did you play video games this week?",
+    answers: [
+      {
+        name: "6 hours or more",
+        score: false,
+      },
+      {
+        name: "4 hours",
+        score: false,
+      },
+      {
+        name: "2 hours or less",
+        score: true,
+      },
+      {
+        name: "i don't play video games",
+        score: true,
+      },
+    ],
+  },
+  {
+    name: "which one is the glorious leader?",
+    answers: [
+      {
+        name: "Xi Jinping",
+        score: true,
+      },
+      {
+        name: "Vladimir Putin",
+        score: false,
+      },
+      {
+        name: "Joe Biden",
+        score: false,
+      },
+    ],
+  },
+  {
+    name: "what brand phone do you have?",
+    answers: [
+      {
+        name: "apple",
+        score: false,
+      },
+      {
+        name: "huawei",
+        score: true,
+      },
+      {
+        name: "samsung",
+        score: false,
+      },
+    ],
+    result: "chinese brand phones are best",
+  },
+  {
+    name: `what do you think of this picture? <br> <img class="xi" src="${root}pooh.png">`,
+    answers: [
+      {
+        name: "amusing",
+        score: false,
+      },
+      {
+        name: "unacceptable",
+        score: true,
+      },
+      {
+        name: "funny",
+        score: false,
+      },
+      {
+        name: "bing chilling",
+        score: false,
+      },
+    ],
+  },
+  {
+    name: "how many kids do you have?",
+    answers: [
+      {
+        name: "no kids",
+        score: true,
+      },
+      {
+        name: "1",
+        score: true,
+      },
+      {
+        name: "2",
+        score: true,
+      },
+      {
+        name: "more than 3",
+        score: false,
+      },
+    ],
+  },
+];
+questions = shuffle(questions);
+const $ = document.querySelectorAll.bind(document);
+
+let quesiter = 0,
+  container = document.querySelector(".container"),
+  socCred = 0,
+  endScreen = function () {
+    if (socCred < 300) {
+      play(1);
+      emoji(1);
+      document.body.classList.add("ohnorun");
+      return `
+        <h1>you have failed the test.</h1><p>your social credit score is ${socCred}. ${
+        Math.random > 0.5
+          ? "Xi Jinping will personally order you to be banned from China. "
+          : "you have been sentenced to a lifetime ban from china by our supreme Xi Jinping. "
+      } you are not loyal enough to our glorious nation.`;
+    } else {
+      emoji(0, 3);
+      document.body.classList.add("passed");
+      return `<h1>you have been proven to be a loyal citizen.</h1><p>your social credit score is ${socCred}. glory to the ccp!</p>`;
+    }
+  };
+
+function play(n) {
+  $("audio")[+!n].pause();
+  $("audio")[n].play();
+}
+
+function nosound() {
+  window.play = (n) => null;
+}
+
+function beginTest() {
+  play(0);
+  let switchTo = 0;
+  if (questions.length != 1) switchSlides(switchTo);
+  else switchSlides(endScreen());
+}
+
+function switchSlides(question) {
+  container.classList.add("transitioning");
+  if (questions.length === 0) {
+    setTimeout(function () {
+      container.innerHTML = endScreen();
+      container.classList.remove("transitioning");
+    }, 600);
+  } else {
+    quesiter++;
+    setTimeout(function () {
+      container.innerHTML = getQuestionHTML(question);
+      container.classList.remove("transitioning");
+    }, 600);
+  }
+}
+
+function getQuestionHTML(ind) {
+  let questionHTML = `
+    <small>question ${quesiter}</small><br><h1>${questions[ind].name}</h1><div class="buttons">`;
+  for (let i = 0; i < questions[ind].answers.length; i++) {
+    questionHTML += `<button onclick="answer(${questions[ind].answers[i].score},${ind})">${questions[ind].answers[i].name}</button>`;
+  }
+  questionHTML += `</div>`;
+  return questionHTML;
+}
+const emoji = (type, num) => {
+  let i = 0;
+  let src = root + (type === 0 ? "thumbsup.png" : "pop.png");
+  if (num)
+    do {
+      worker(i);
+    } while (i++ < num - 1);
+
+  function worker(i) {
+    if (i >= num) return;
+    let x = Math.floor(Math.random() * (window.innerWidth - 300));
+    let y = Math.floor(Math.random() * (window.innerHeight - 300));
+    let emoji = document.createElement("IMG");
+    emoji.src = src;
+    emoji.style.left = x + "px";
+    emoji.style.top = y + "px";
+    emoji.classList.add("chinese-emoji");
+    document.body.appendChild(emoji);
+  }
+  worker();
+};
+
+function answer(positive, i) {
+  let incnum = 100 + Math.round(Math.random() * 50);
+  if (!positive) incnum *= -1;
+  emoji(+!positive);
+  socCred += incnum;
+  msg(incnum, positive, questions[i].result);
+  $(".counter")[0].innerHTML = `social credit: ${socCred}`;
+  questions.shift();
+}
+
+function msg(mesg, pos, result) {
+  let toast = document.createElement("DIV");
+  toast.classList.add("toast");
+  toast.innerHTML = `${pos === true ? "+" : ""}${mesg} SOCIAL CREDIT <br><p>${
+    result || ""
+  }</p><br><button onclick="switchSlides(0);purge(${pos})"">NEXT</button>`;
+
+  document.body.appendChild(toast);
+}
+
+function purge(pos) {
+  let emojis = $(".chinese-emoji");
+  if (pos === false) emojis.forEach((e) => e.remove());
+  else emojis[0].remove();
+  document.querySelector(".toast").remove();
+}
--- wiggle/bezier.js
+++ wiggle/bezier.js
@@ -20,46 +20,132 @@
  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
  * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-var cubicBezier = function() {
-    "use strict";
-    var r = function(r, n, e, t) {
-        var u = 3 * r,
-            i = 3 * (e - r) - u,
-            a = 1 - u - i,
-            f = 3 * n,
-            c = 3 * (t - n) - f,
-            o = 1 - f - c,
-            s = function(r) {
-                return ((a * r + i) * r + u) * r
-            },
-            b = function(r, n) {
-                return e = function(r, n) {
-                    var e, t, f, c, o, b, v;
-                    for (f = r, b = 0; b < 8; b++) {
-                        if (c = s(f) - r, Math.abs(c) < n) return f;
-                        if (o = (3 * a * (v = f) + 2 * i) * v + u, Math.abs(o) < 1e-6) break;
-                        f -= c / o
-                    }
-                    if ((f = r) < (e = 0)) return e;
-                    if (f > (t = 1)) return t;
-                    for (; e < t;) {
-                        if (c = s(f), Math.abs(c - r) < n) return f;
-                        r > c ? e = f : t = f, f = .5 * (t - e) + e
-                    }
-                    return f
-                }(r, n), ((o * e + c) * e + f) * e;
-                var e
-            };
-        return function(r, n) {
-            return b(r, function(r) {
-                return 1 / (200 * r)
-            }(+n || 400))
-        }
-    };
-    return function(obj) {
-        return r(obj.points[0], obj.points[1], obj.points[2], obj.points[3])(obj.toEase, obj.a)
+
+const CUBIC_BEZIER_SPLINE_SAMPLES = 11;
+const kBezierEpsilon = 1e-7;
+const kMaxNewtonIterations = 4;
+
+class UnitBezier {
+  constructor(p1x, p1y, p2x, p2y) {
+    // Calculate the polynomial coefficients, implicit first and last
+    // control points are (0,0) and (1,1).
+    this.cx = 3 * p1x;
+    this.bx = 3 * (p2x - p1x) - this.cx;
+    this.ax = 1 - this.cx - this.bx;
+
+    this.cy = 3 * p1y;
+    this.by = 3 * (p2y - p1y) - this.cy;
+    this.ay = 1 - this.cy - this.by;
+
+
+    // End-point gradients are used to calculate timing function results
+    // outside the range [0, 1].
+    //
+    // There are four possibilities for the gradient at each end:
+    // (1) the closest control point is not horizontally coincident with regard to
+    //     (0, 0) or (1, 1). In this case the line between the end point and
+    //     the control point is tangent to the bezier at the end point.
+    // (2) the closest control point is coincident with the end point. In
+    //     this case the line between the end point and the far control
+    //     point is tangent to the bezier at the end point.
+    // (3) both internal control points are coincident with an endpoint. There
+    //     are two special case that fall into this category:
+    //     CubicBezier(0, 0, 0, 0) and CubicBezier(1, 1, 1, 1). Both are
+    //     equivalent to linear.
+    // (4) the closest control point is horizontally coincident with the end
+    //     point, but vertically distinct. In this case the gradient at the
+    //     end point is Infinite. However, this causes issues when
+    //     interpolating. As a result, we break down to a simple case of
+    //     0 gradient under these conditions.
+    if (p1x > 0)
+      this.startGradient = p1y / p1x;
+    else if (!p1y && p2x > 0)
+      this.startGradient = p2y / p2x;
+    else if (!p1y && !p2y)
+      this.startGradient = 1;
+    else
+      this.startGradient = 0;
+    if (p2x < 1)
+      this.endGradient = (p2y - 1) / (p2x - 1);
+    else if (p2y == 1 && p1x < 1)
+      this.endGradient = (p1y - 1) / (p1x - 1);
+    else if (p2y == 1 && p1y == 1)
+      this.endGradient = 1;
+    else
+      this.endGradient = 0;
+
+    this.deltaT = 1.0 / (CUBIC_BEZIER_SPLINE_SAMPLES - 1);
+    this.splineSamples = [];
+    for (let i = 0; i < CUBIC_BEZIER_SPLINE_SAMPLES; i++)
+      this.splineSamples[i] = this.sampleCurveX(i * this.deltaT);
+  }
+
+  sampleCurveX(t) {
+    // `ax t^3 + bx t^2 + cx t' expanded using Horner's rule.
+    return ((this.ax * t + this.bx) * t + this.cx) * t;
+  }
+
+  sampleCurveY(t) {
+    return ((this.ay * t + this.by) * t + this.cy) * t;
+  }
+
+  sampleCurveDerivativeX(t) {
+    return (3 * this.ax * t + 2 * this.bx) * t + this.cx;
+  }
+
+  // Given an x value, find a parametric value it came from.
+  solveCurveX(x, epsilon) {
+    let t0 = 0, t1 = 0, t2 = x, x2 = 0, d2 = 0, i = 0;
+
+    // Linear interpolation of spline curve for initial guess.
+    for (i = 1; i < CUBIC_BEZIER_SPLINE_SAMPLES; i++) {
+      if (x <= this.splineSamples[i]) {
+        t1 = this.deltaT * i;
+        t0 = t1 - this.deltaT;
+        t2 = t0 + (t1 - t0) * (x - this.splineSamples[i - 1]) / (this.splineSamples[i] - this.splineSamples[i - 1]);
+        break;
+      }
     }
-}();
+
+    // Perform a few iterations of Newton's method -- normally very fast.
+    // See https://en.wikipedia.org/wiki/Newton%27s_method.
+    let newtonEpsilon = Math.min(kBezierEpsilon, epsilon);
+    for (i = 0; i < kMaxNewtonIterations; i++) {
+      x2 = this.sampleCurveX(t2) - x;
+      if (Math.abs(x2) < newtonEpsilon)
+        return t2;
+      d2 = this.sampleCurveDerivativeX(t2);
+      if (Math.abs(d2) < kBezierEpsilon)
+        break;
+      t2 = t2 - x2 / d2;
+    }
+    if (Math.abs(x2) < epsilon)
+      return t2;
+
+    // Fall back to the bisection method for reliability.
+    while (t0 < t1) {
+      x2 = this.sampleCurveX(t2);
+      if (Math.abs(x2 - x) < epsilon)
+        return t2;
+      if (x > x2)
+        t0 = t2;
+      else
+        t1 = t2;
+      t2 = (t1 + t0) * .5;
+    }
+
+    // Failure.
+    return t2;
+  }
+
+  solve(x, epsilon) {
+    if (x < 0.0)
+      return 0.0 + this.startGradient * x;
+    if (x > 1.0)
+      return 1.0 + this.endGradient * (x - 1.0);
+    return this.sampleCurveY(this.solveCurveX(x, epsilon));
+  }
+}
