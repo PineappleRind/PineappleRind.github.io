dacba002eab7655ff29614e923867e5f00068c03
pr <pineapplerind.info@gmail.com>
2026-07-03 11:09:36 -0400

    Trumpet Truck has arrived.

--- home/src/main.ts
+++ home/src/main.ts
@@ -19,7 +19,7 @@ for (const [i, element] of Array.from(
 
 (() => {
 	if (window.location.pathname !== "/") return;
-	const date = () => new Date("2026-07-02T18:00:00Z").toLocaleDateString();
+	const date = () => new Date("2026-07-03T12:00:00Z").toLocaleDateString();
 	$("#insertDateHerePls").innerText = date();
 
 	let artistURLs = {
@@ -32,17 +32,42 @@ for (const [i, element] of Array.from(
 		"Deezer (incomplete)": "https://www.deezer.com/artist/158442482",
 		Pandora: "https://www.pandora.com/artist/pineapplerind/ARgXdmk9wgPVqbX",
 	};
+	let trumpetTruckURLs = {
+		Spotify: "https://open.spotify.com/album/7Cn019R2XVT1IIkzqFTTku",
+		Apple_Music:
+			"https://music.apple.com/ca/album/trumpet-truck-single/6780703535",
+		YouTube_Music:
+			"https://music.youtube.com/playlist?list=OLAK5uy_mLk-78VLWTJhAY_hyEisL71oMp2FhKjv8",
+		Tidal: "https://tidal.com/album/533801280",
+		Amazon_Music: "https://music.amazon.com/albums/B0H5J1G6S6",
+		"No deezer :(": null,
+		Pandora:
+			"https://www.pandora.com/artist/pineapplerind/trumpet-truck/AL66pnzK3hh2jlw",
+	};
 
+	//2lazy2generalizethis
 	const select = $("#artistPage");
+	const $TRUMPETTRUCKYAY = $("#trumpetTruckSelect");
+
 	for (const key in artistURLs) {
 		const opt = document.createElement("option");
 		opt.innerText = key.replaceAll("_", " ");
 		opt.value = key;
 		select.append(opt);
 	}
-	select.oninput = (e) => {
-		const { value } = select;
-		const url = artistURLs[value];
-		window.open(url, "_blank");
+	for (const key in trumpetTruckURLs) {
+		const opt = document.createElement("option");
+		opt.innerText = key.replaceAll("_", " ");
+		opt.value = key;
+		$TRUMPETTRUCKYAY.append(opt);
+	}
+
+	select.oninput = () => {
+		const url = artistURLs[select.value];
+		if (url) window.open(url, "_blank");
+	};
+	$TRUMPETTRUCKYAY.oninput = () => {
+		const url = trumpetTruckURLs[$TRUMPETTRUCKYAY.value];
+		if (url) window.open(url, "_blank");
 	};
 })();
